36405d450ef53f0a8988c877a2e49c6639d7a295
Jeremy Valance <jvalance@pcm-jvalance-1.local>
2017-10-25 18:05:52 -0400

    Added login request

---
+++ src/blackDuckLogin.ts
@@ -0,0 +1,56 @@
+import * as vscode from 'vscode';
+import request = require('request-promise');
+
+let _response: Response;
+
+
+export interface Response {
+    response: Object;
+}
+
+export async function blackDuckLogin():  Promise<{ hubUrl: string, username: string, password: string, response: Object }> {
+
+    const hubUrl: string = await vscode.window.showInputBox({ ignoreFocusOut: true, prompt: 'Hub Url' });
+    if (hubUrl) {
+        const username: string = await vscode.window.showInputBox({ ignoreFocusOut: true, prompt: 'Username' });
+        if (username) {
+            const password: string = await vscode.window.showInputBox({ ignoreFocusOut: true, prompt: 'Password', password: true });
+            if (password) {
+                _response = await login(hubUrl, username, password);
+                if (_response) {
+                    console.log("Success", _response);
+                    return { hubUrl: hubUrl, username: username, password: password, response: <string>_response.response };
+                }
+            }
+        }
+    }
+
+    return;
+}
+
+
+async function login(hubUrl: string, username: string, password: string) : Promise<Response> {
+    let r: Response;
+
+    let options = {
+        method: 'POST',
+        uri: hubUrl + ':443/j_spring_security_check',
+        form: {
+            j_username: username,
+            j_password: password
+        },
+        resolveWithFullResponse: true,
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    try {
+        r = await request(options);
+    } catch (error) {
+        console.log(error);
+        vscode.window.showErrorMessage(error.error.detail);
+    }
+
+    return r;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { blackDuckLogin } from './blackDuckLogin';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,6 +22,14 @@ export function activate(context: vscode.ExtensionContext) {
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
+
+
+    vscode.commands.registerCommand('vs-code-blackduck.login', () => blackDuckLogin());
+    //context.subscriptions.push(vscode.commands.registerCommand('vs-code-blackduck.login', blackDuckLogin));
+
+
+
+
 
     context.subscriptions.push(disposable);
 }
